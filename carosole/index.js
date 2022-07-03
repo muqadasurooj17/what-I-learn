6a54d8a677f0d44d085003db52d7dd8c45de1be8
muqadasurooj/17 <muqadasurooj123@gmail.com>
2022-07-03 13:17:24 +0500

    created carosole in react

---
+++ carosole/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+//import Image from "next/image";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
+import Carosole from "../pages/Carosole";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Newyork</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured/>
+      <PizzaList/>
+   
+<Carosole/>
+    </div>
+  );
+}
