5dc7ddf8bfd4c5e91c7c8331f291a5b2225d00a0
Muqadas Urooj <muqadasurooj123@gmail.com>
2023-10-06 18:24:56 +0500

    commit of the day

--- Allabout react/HOC.jsx
+++ Allabout react/HOC.jsx
@@ -1,3 +1,12 @@
 // a higher-order component is a function that takes a 
 //component as an argument and returns a new component that wraps 
 //the original component
+
+
+// In React, a higher-order component is a function that takes a component as an argument and returns a 
+// new component that wraps the original component.
+
+// HOCs allow you to add additional functionality to a component without modifying the component's code. For example, you can use a HOC to add authentication or routing capabilities to a component or to apply a specific style or behavior to multiple components.
+
+// HOCs can take additional arguments, which lets you customize the behavior of the HOC. This makes them 
+// a flexible and reusable way to add functionality to your components.
